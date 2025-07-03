5fcc271db7b71925c9cf87cfac7c018577895ccf
RussianHusky <russianhusky0208@outlook.com>
2025-07-03 23:28:45 +0300

    224

---
+++ LeetCode/LC224/LeetCode224.cs
@@ -0,0 +1,178 @@
+﻿namespace LeetCode.LC224;
+
+public class LeetCode224
+{
+    public int Calculate(string s) 
+{
+    if (int.TryParse(s, out int m))
+    {
+        return m;
+    }
+
+    int res = 0;
+    char operand = ' ';
+    
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (char.IsDigit(s[i]))
+        {
+            if (i + 1 < s.Length && char.IsDigit(s[i + 1]))
+            {
+                switch (operand)
+                {
+                    case '+':
+                        res += MultyDigit(s, ref i);
+                        break;
+
+                    case '-':
+                        res -= MultyDigit(s, ref i);
+                        break;
+
+                    case ' ':
+                        res = MultyDigit(s, ref i);
+                        break;
+                }
+            }
+            else if (operand == ' ') 
+            {
+                res = s[i] - 48;
+            }
+            else
+            {
+                switch (operand)
+                {
+                    case '+':
+                        res += s[i] - 48;
+                        break;
+                    case '-':
+                        res -= s[i] - 48;
+                        break;
+                }
+            }
+        }
+        else
+        {
+            switch (s[i])
+            {
+                case ' ':
+                    continue;
+
+                case '+':
+                    operand = '+';
+                    break;
+
+                case '-':
+                    operand = '-';
+                    break;
+
+                case '(':
+                    switch (operand)
+                    {
+                        case '+':
+                            res += Parent(s, ref i);
+                            break;
+
+                        case '-':
+                            res -= Parent(s, ref i);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    return res;
+}
+
+public int Parent(string s, ref int i)
+{
+    char operand = ' ';
+    int insideParent = 0;
+
+    while (i < s.Length)
+    {
+        if (char.IsDigit(s[i]))
+        {
+            if (i + 1 < s.Length && char.IsDigit(s[i + 1]))
+            {
+                switch (operand)
+                {
+                    case '+':
+                        insideParent += MultyDigit(s, ref i);
+                        break;
+
+                    case '-':
+                        insideParent -= MultyDigit(s, ref i);
+                        break;
+                }
+            }
+            else if (operand == ' ') 
+            {
+                insideParent = s[i] - 48;
+            }
+            else
+            {
+                switch (operand)
+                {
+                    case '+':
+                        insideParent += s[i] - 48;
+                        break;
+                    case '-':
+                        insideParent -= s[i] - 48;
+                        break;
+                }
+            }
+        }
+        else
+        {
+            switch (s[i])
+            {
+                case '+':
+                    operand = '+';
+                    break;
+
+                case '-':
+                    operand = '-';
+                    break;
+
+                case '(':
+                    switch (operand)
+                    {
+                        case '+':
+                            insideParent += Parent(s, ref i);
+                            break;
+
+                        case '-':
+                            insideParent -= Parent(s, ref i);
+                            break;
+                    }
+                    break;
+
+                case ')':
+                    return insideParent;
+            }
+        }
+        i++; // Increment to next character
+    }
+
+    return insideParent;
+}
+
+public int MultyDigit(string s, ref int i)
+{
+    int res = 0;
+
+    while (i < s.Length)
+    {
+        if (!char.IsDigit(s[i]))
+        {
+            i--;
+            break;
+        }
+        res = res * 10 + (s[i] - 48);
+        i++;
+    }
+
+    return res;
+}
+}
--- LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net9.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+768bf8714026e810db53a2925731c0e92383d7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b5968008bf2b8908e1be7e9b28246eaa080655")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
